a3a3357b18c1be6fabc9692a361f7e44e914a75e
David Begin <davidmichaelbe@gmail.com>
2023-04-21 13:32:09 -0700

    Fixes to filepaths for duests

--- animate_scripts/animate.sh
+++ animate_scripts/animate.sh
@@ -7,58 +7,60 @@ VOICE_FILE="$1"
 DIALOGUE_FILE="$2"
 NAMESPACE="$3"
 
+MOV_FILE="${VOICE_FILE}.mov"
+FINAL_VIDEO="${VOICE_FILE}.mp4"
+
 echo "Generating Animation for: $VOICE_FILE | $DIALOGUE_FILE | $NAMESPACE"
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 PARENT_DIR="$( cd "${SCRIPT_DIR}/.." && pwd )"
-LOG_FILE=$PARENT_DIR/tmp/animation.log
-TMP_ANIMATIONS_FOLDER=$PARENT_DIR/tmp/animations/$NAMESPACE
 
-MOV_FILE="${VOICE_FILE}.mov"
-# MOV_FILE=$(echo $VOICE_FILE | sed "s/.wav/.mov/")
-SOUND_FILE="${PARENT_DIR}/tmp/voices/${NAMESPACE}/${VOICE_FILE}.wav"
-FINAL_VIDEO="${NAMESPACE}.mp4"
+LOG_FILE=$PARENT_DIR/tmp/animation.log
+
+VOICES_DIR="${PARENT_DIR}/tmp/voices/${NAMESPACE}"
+ANIMATIONS_DIR="${PARENT_DIR}/tmp/animations/${NAMESPACE}/${VOICE_FILE}"
+SOUND_FILE="${VOICES_DIR}/${VOICE_FILE}.wav"
+
+mkdir -p $ANIMATIONS_DIR
 
 echo "Paths: $MOV_FILE | $SOUND_FILE | $FINAL_VIDEO"
 
-echo $TMP_ANIMATIONS_FOLDER
-
-mkdir -p $TMP_ANIMATIONS_FOLDER
+# ===========================================
 
 echo "Starting Lip Sync" >> $LOG_FILE
 
-echo "$SCRIPT_DIR/lip_sync.sh $VOICE_FILE $DIALOGUE_FILE $NAMESPACE"
-# $SCRIPT_DIR/lip_sync.sh $VOICE_FILE $DIALOGUE_FILE $NAMESPACE
+echo "$SCRIPT_DIR/lip_sync.sh $VOICE_FILE $DIALOGUE_FILE"
+$SCRIPT_DIR/lip_sync.sh $VOICE_FILE $DIALOGUE_FILE $VOICES_DIR $ANIMATIONS_DIR
 
 echo "Finished Lip Sync" >> $LOG_FILE
 
 awk 'NR!=1 {print int(($1*24)-1)}' \
-  $TMP_ANIMATIONS_FOLDER/output.txt > $TMP_ANIMATIONS_FOLDER/output2.txt
+  $ANIMATIONS_DIR/output.txt > $ANIMATIONS_DIR/output2.txt
 
 # # add last record
-awk 'END{print int($1*24)}' $TMP_ANIMATIONS_FOLDER/output.txt >> $TMP_ANIMATIONS_FOLDER/output2.txt
+awk 'END{print int($1*24)}' $ANIMATIONS_DIR/output.txt >> $ANIMATIONS_DIR/output2.txt
 
 # # combine the 2 files
-awk 'NR==FNR{a[NR]=$0;next}{print a [FNR],$0}' $TMP_ANIMATIONS_FOLDER/output2.txt $TMP_ANIMATIONS_FOLDER/output.txt > $TMP_ANIMATIONS_FOLDER/output3.txt
+awk 'NR==FNR{a[NR]=$0;next}{print a [FNR],$0}' $ANIMATIONS_DIR/output2.txt $ANIMATIONS_DIR/output.txt > $ANIMATIONS_DIR/output3.txt
 
-awk -v tmp_folder="$TMP_ANIMATIONS_FOLDER" '{print "for ((i="int($2*24)"; i<="$1"; i++)); do cp /home/begin/code/BeginGPT/mouths/"$3".png "tmp_folder"/output_$(printf %05d $i).png ; done "}' "$TMP_ANIMATIONS_FOLDER/output3.txt" > "$TMP_ANIMATIONS_FOLDER/output.sh"
+awk -v tmp_folder="$ANIMATIONS_DIR" '{print "for ((i="int($2*24)"; i<="$1"; i++)); do cp /home/begin/code/BeginGPT/mouths/"$3".png "tmp_folder"/output_$(printf %05d $i).png ; done "}' "$ANIMATIONS_DIR/output3.txt" > "$ANIMATIONS_DIR/output.sh"
 
 # # add #! /bin/bash to output.sh
-chmod +x $TMP_ANIMATIONS_FOLDER/output.sh
+chmod +x $ANIMATIONS_DIR/output.sh
 
-$TMP_ANIMATIONS_FOLDER/output.sh
+$ANIMATIONS_DIR/output.sh
 
-# echo "STARTING FFMPEG 1" >> $LOG_FILE
-# ffmpeg -y -framerate 24                     \
-#   -i $TMP_ANIMATIONS_FOLDER/output_%05d.png \
-#   -vcodec png                               \
-#   $TMP_ANIMATIONS_FOLDER/$MOV_FILE
-# echo "Finished FFMPEG 1" >> $LOG_FILE
+echo "STARTING FFMPEG 1" >> $LOG_FILE
+ffmpeg -y -framerate 24                     \
+  -i $ANIMATIONS_DIR/output_%05d.png \
+  -vcodec png                               \
+  $ANIMATIONS_DIR/$MOV_FILE
+echo "Finished FFMPEG 1" >> $LOG_FILE
 
 
 echo "STARTING FFMPEG 2" >> $LOG_FILE
 ffmpeg -y \
-  -i $TMP_ANIMATIONS_FOLDER/$MOV_FILE \
+  -i $ANIMATIONS_DIR/$MOV_FILE \
   -i $SOUND_FILE                             \
   -map 1:0                                   \
   -map 0:0                                   \
@@ -66,6 +68,6 @@ ffmpeg -y \
 echo "Finishing FFMPEG 2" >> $LOG_FILE
 
 # Delete all previous output mouth files
-# rm $TMP_ANIMATIONS_FOLDER/output_*.png
+# rm $ANIMATIONS_DIR/output_*.png
 
 # beginbot "!reload_rapper"
